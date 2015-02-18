9bfc4fabb1bcaf19398b96a7178c942e61696d93
Caitlin Guzman <cg1656@nyu.edu>
2015-02-18 11:56:54 -0500

    FINALLY FIGURED OUT HOW TO REFER TO GAME OBJECTS IN C#. ABOUT TIME! I still didn't color the objects, but I don't even care

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,19 +8,15 @@ public class PlayerMovement : MonoBehaviour {
 	void Start () {
 		
 	}
-
-	string textDisplay = "SANTA";
-
+	
 	// Update is called once per frame
 	void Update () {
-
-		string textBuffer = textDisplay;
 
 		// move forward if I press W
 		if ( Input.GetKey (KeyCode.W) ) {
 			//Time.deltaTime is the fraction of a second in between a frame
 			//a value that gets bigger with low FPS, smaller with high FPS
-			transform.position += new Vector3 (0f, 0f, 10f) * Time.deltaTime;
+			transform.position += new Vector3 (0f, 0f, 10f) * (Time.deltaTime*10);
 		}
 		
 		// FRAMERATE-DEPENDENT BEHAVIOR
@@ -32,34 +28,18 @@ public class PlayerMovement : MonoBehaviour {
 		
 		// move backward if I press S
 		if ( Input.GetKey (KeyCode.S) ) {
-			transform.position += new Vector3 (0f, 0f, -10f) * Time.deltaTime;
+			transform.position += new Vector3 (0f, 0f, -10f) * (Time.deltaTime*10);
 		}
 		
 		// move left if I press A
 		if ( Input.GetKey (KeyCode.A) ) {
-			transform.position += new Vector3 (-10f, 0f, 0f) * Time.deltaTime;
+			transform.position += new Vector3 (-10f, 0f, 0f) * (Time.deltaTime*10);
 		}
 		
 		// move right if I press D
 		if ( Input.GetKey (KeyCode.D) ) {
-			transform.position += new Vector3 (10f, 0f, 0f) * Time.deltaTime;
+			transform.position += new Vector3 (10f, 0f, 0f) * (Time.deltaTime*10);
 		}
-
-		// get the treasure if I press Space
-		// if player is close to treasure {
-			//if ( Input.GetKey (KeyCode.Space) ) {
-				//PLAYER WINS
-			//}
-		//}
-
-		// write text if I get close to treasure
-		if ( textDisplay == "SANTA") {
-			textBuffer += "\nA gaudy, dark dressing room in a well-respected theatre. You'll have to look hard if you hope to find anything in all the clutter.";
-			textBuffer += "\n\nPress [X] to examine the dressing room.";
-		}
-		//
-
-		GetComponent<Text>().text = textBuffer;
 		
 	}
 }
--- Assets/TextDisplay.cs
+++ Assets/TextDisplay.cs
@@ -4,15 +4,21 @@ using UnityEngine.UI;
 
 public class TextDisplay : MonoBehaviour {
 
+	public GameObject Sled;
+	public GameObject Gift;
+	
 	// Use this for initialization
 	void Start () {
-	
+		Sled = GameObject.Find("Sled");
+		Gift = GameObject.Find ("Gift");
+		
 	}
 	
 	string proximity = "Oh no!";
 	
 	// Update is called once per frame
 	void Update () {
+		float dist = Vector3.Distance(Gift.transform.position,(Sled.transform.position));
 
 		string textBuffer = proximity;
 
@@ -27,23 +33,55 @@ public class TextDisplay : MonoBehaviour {
 
 		if ( proximity == "Cold...") {
 			textBuffer += "\nYou're not very close.";
-		}
 
+			if ( dist < 1500 ) {
+				proximity = "Warm...";
+			}
+		}
+		
 		if ( proximity == "Warm...") {
 			textBuffer += "\nYou're onto something.";
+
+			if ( dist < 1200 ) {
+				proximity = "Warmer...";
+			}
+
+			if ( dist > 1500 ) {
+				proximity = "Cold...";
+			}
 		}
 
 		if ( proximity == "Warmer...") {
 			textBuffer += "\nYou're nearly there!";
+
+			if ( dist < 900 ) {
+				proximity = "Hot...";
+			}
+
+			if ( dist > 1200 ) {
+				proximity = "Warm...";
+			}
 		}
 
 		if ( proximity == "Hot...") {
 			textBuffer += "\nSo close!";
+
+			if ( dist < 600 ) {
+				proximity = "On fire!";
+			}
+
+			if ( dist > 900 ) {
+				proximity = "Warmer...";
+			}			
 		}
 
 		if ( proximity == "On fire!") {
-			textBuffer += "\nYou've found it! Press space to pick up the present!";
+			textBuffer += " You've found it! Press space to pick up the present!";
 
+			if ( dist > 600 ) {
+				proximity = "Hot...";
+			}
+			
 			if (Input.GetKeyDown (KeyCode.Space)) {
 				proximity = "You win!";
 			}
