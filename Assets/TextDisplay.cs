8c6525a2391b2696b8c63e0b43a530c95bc3cbc0
Caitlin Guzman <cg1656@nyu.edu>
2015-02-18 01:25:39 -0500

    Added fog, fixed camera to UFO. To do: get text to respond to location of sled re: proximity to treasure

---
+++ Assets/TextDisplay.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextDisplay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	string proximity = "Oh no!";
+	
+	// Update is called once per frame
+	void Update () {
+
+		string textBuffer = proximity;
+
+		if ( proximity == "Oh no!") {
+			textBuffer += " You've dropped a present from the sled! You'd better hurry to retrieve it before Christmas day arrives!\nPress WASD to move Santa's sled.";
+		
+			if ((Input.GetKeyDown (KeyCode.W))||(Input.GetKeyDown (KeyCode.S))||(Input.GetKeyDown (KeyCode.A))||(Input.GetKeyDown (KeyCode.D))) {
+				proximity = "Cold...";
+			}
+		
+		}
+
+		if ( proximity == "Cold...") {
+			textBuffer += "\nYou're not very close.";
+		}
+
+		if ( proximity == "Warm...") {
+			textBuffer += "\nYou're onto something.";
+		}
+
+		if ( proximity == "Warmer...") {
+			textBuffer += "\nYou're nearly there!";
+		}
+
+		if ( proximity == "Hot...") {
+			textBuffer += "\nSo close!";
+		}
+
+		if ( proximity == "On fire!") {
+			textBuffer += "\nYou've found it! Press space to pick up the present!";
+
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				proximity = "You win!";
+			}
+		}
+
+		if ( proximity == "You win!") {
+			textBuffer += "\nYay";
+		}
+		
+		//
+		
+		GetComponent<Text>().text = textBuffer;
+	}
+}
